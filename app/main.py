3940dd376165a3e8eff647b918566ab806d7cbdf
Your Name <you@example.com>
2025-11-10 11:37:29 +0700

    fixed: Constructor Logic, Method Usage, Parameter Naming, Type Hinting

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: iter = None) -> None:
-        super().__init__(name, weight)
+        super().__init__(name, weight, coords)
         if coords is None or len(coords) != 3:
             self.coords = [0, 0, 0]
         else:
@@ -54,7 +54,9 @@ class DeliveryDrone(FlyingRobot):
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
+        self.current_load = None
+        if current_load is not None:
+            self.hook_load(current_load)
 
     def hook_load(self, load: Cargo) -> None:
         if self.current_load is None and load.weight <= self.max_load_weight:
